1a30ed8fc79848c3ff933b66be7a85708b9e71ad
akash kumar <120650503+AKASH2017AMITY@users.noreply.github.com>
2024-02-18 16:14:28 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,11 @@
-const asynHandler = (req,res) => {
-    (req,res,next)=> {
-
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
 
     }
 }
 
-export default asynHandler
+export { asyncHandler }
 
 
 // const asynHandler = () => {}
